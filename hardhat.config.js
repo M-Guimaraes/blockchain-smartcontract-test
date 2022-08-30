3c8314c074be0c8d2a3141e03abff647f8c4f98e
Marcelo Guimaraes da Silva <marcelo.silva@toroinvestimentos.com.br>
2022-08-30 20:04:58 -0300

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+
+module.exports = {
+  solidity: "0.8.9",
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4
+    }
+  }
+};
